2a795c40665c3434118c27eb42b252619627adfe
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 05:34:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,6 @@ def sort_array_asc (array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1])
 
 def sort_array_desc (array)
   array.sort do |left, right|
@@ -10,5 +9,5 @@ def sort_array_desc (array)
   end
 end
 
-sort_array_asc([25, 7, 1])
+
 
