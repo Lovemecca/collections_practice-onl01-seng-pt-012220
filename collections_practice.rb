6f03cdb20a5bddce915b0f2603cedc61d69e7ec0
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 05:32:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,8 +2,12 @@ def sort_array_asc (array)
   array.sort
 end
 
+sort_array_asc([25, 7, 1])
+
 def sort_array_desc (array)
   array.sort do |left, right|
     right <=> left
 end
+
+sort_array_asc([25, 7, 1])
 
